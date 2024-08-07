f57eef2372ac3c2f52490b395d1f5748d71cff14
HyeonProG <guswo3443@naver.com>
2024-08-07 17:21:39 +0900

    css 추가 및 mainController 기능 추가

--- src/main/java/com/tenco/bank/controller/MainController.java
+++ src/main/java/com/tenco/bank/controller/MainController.java
@@ -1,5 +1,24 @@
 package com.tenco.bank.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // IoC 대상(싱글톤 패턴으로 관리) - 제어의 역전 개념
 public class MainController {
 
+	// REST API 기반으로 주소 설계 가능
+	
+	// 주소 설계
+	// http://localhost:8080/main-page
+	
+	@GetMapping({"/main-page", "/index"})
+	public String mainPage() {
+		System.out.println("mainPage() 호출 확인");
+		// [JSP 파일 찾기(yml 설정)] - view resolver
+		// prefix: /WEB-INF/view
+		//				   /main
+		// suffix: .jsp
+		return "/main";
+	}
+	
 }
