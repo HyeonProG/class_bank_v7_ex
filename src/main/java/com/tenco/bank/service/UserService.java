e07bec9e86d627a9b232289a94a8ced831394134
HyeonProG <guswo3443@naver.com>
2024-08-08 16:37:06 +0900

    h2 스키마 처리 및 회원가입 기능 구현

---
+++ src/main/java/com/tenco/bank/service/UserService.java
@@ -0,0 +1,37 @@
+package com.tenco.bank.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tenco.bank.dto.SignUpDTO;
+import com.tenco.bank.handler.exception.DataDeliveryException;
+import com.tenco.bank.handler.exception.RedirectException;
+import com.tenco.bank.repository.interfaces.UserRepository;
+
+@Service // IoC 대상(싱글톤 패턴) - 제어의 역전
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Transactional
+	public void createUser(SignUpDTO dto) {
+		int result = 0;
+		try {
+			result = userRepository.insert(dto.toUser());
+		} catch (DataAccessException e) {
+			throw new DataDeliveryException("잘못된 처리 입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (Exception e) {
+			throw new RedirectException("알 수 없는 오류", HttpStatus.SERVICE_UNAVAILABLE);
+		}
+		
+		if (result != 1) {
+			throw new DataDeliveryException("회원 가입 실패", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	}
+	
+}
